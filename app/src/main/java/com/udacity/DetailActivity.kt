79ca3f8c950c7a102dab8b05c0e0d8ede224ce10
Gregor Lennie <gregorlennie@hotmail.co.uk>
2021-10-23 16:48:29 +0100

    Add DetailActivity layout

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -13,6 +15,18 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        btnOk.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finishAndRemoveTask()
+        }
+
+        val file = intent.getStringExtra("fileName").toString()
+        val status = intent.getStringExtra("status").toString()
+
+        fileNameText.text = file
+        statusText.text = status
     }
 
 }
